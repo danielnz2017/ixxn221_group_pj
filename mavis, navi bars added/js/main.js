4059e2e1cbeac0a4dac48e513420174106be983c
Daniel (JH) Huang <32477164+danielnz2017@users.noreply.github.com>
2020-10-14 19:47:24 +1300

    daniel_navi added_

---
+++ mavis, navi bars added/js/main.js
@@ -0,0 +1,20 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+
+
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
